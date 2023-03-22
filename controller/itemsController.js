e117b1a844aeab83049cb0d377c8171447c0c4a3
Supharoek Kongprapane <pream5138@gmail.com>
2023-03-22 11:20:22 +0700

    add delet

--- controller/itemsController.js
+++ controller/itemsController.js
@@ -49,7 +49,7 @@ exports.addItem = async (req, res) => {
   };
   try {
     const data = await docClient.send(new PutCommand(params));
-    res.send(data.Items);
+    res.send(item);
   } catch (err) {
     console.error(err);
     res.status(500).send(err);
@@ -59,7 +59,15 @@ exports.addItem = async (req, res) => {
 // TODO #1.3: Delete an item from DynamDB
 exports.deleteItem = async (req, res) => {
   const item_id = req.params.item_id;
-
-  // You should change the response below.
-  res.send("This route should delete an item in DynamoDB with item_id.");
+  const params = {
+    Key: { item_id: item_id },
+    TableName: process.env.aws_items_table_name,
+  };
+  try {
+    const data = await docClient.send(new DeleteCommand(params));
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 };
